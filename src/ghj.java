9395d8fc7746cacf6ac521fb3f27a064f8982628
YangPu <1024094376@qq.com>
2021-07-17 00:07:14 +0800

    first commit

---
+++ src/ghj.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName ghj
+ * @Description TODO helloworld
+ * @Author YangPu
+ * @Date 2021/7/16 23:57
+ * @Version 1.0
+ **/
+public class ghj {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
